1b3729c21b9499beae8d009e4658f035d7219759
vtorvieira <vieira.vitor42@gmail.com>
2022-12-29 13:04:18 -0300

    feat: add new style effect

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,8 +1,12 @@
 import React from "react";
-import { MainProject } from "./Style";
+import { FaGithub } from 'react-icons/fa';
+import { FiExternalLink } from 'react-icons/fi';
+import { TbPokeball } from 'react-icons/tb';
+import { Container } from "./Style";
 import AppMovies from '../../svg/AppMovies.svg';
 import AppRecipies from '../../svg/AppRecipies.svg';
-import CashTransfer from '../../svg/CashTransfer.svg';
+import digitalBank from '../../svg/digitalBank.svg';
+import poke from '../../svg/poke.svg';
 import TODOLIST from '../../svg/TODOLIST.svg';
 import BLOGAPI from '../../svg/BLOGAPI.svg';
 import trybeSmith from '../../svg/trybeSmith.svg';
@@ -10,32 +14,94 @@ import trivia from '../../svg/trivia.svg';
 
 function Projects() {
   return (
-    <MainProject>
-      <a className="redirect" href="https://github.com/VtorVieira/transfer-cash" target="_blank" rel="noreferrer">
-        <img className="logo" src={CashTransfer} alt="Cash Transfer" />
-      </a>
-      <a className="redirect" href="https://github.com/VtorVieira/AppMovies" target="_blank" rel="noreferrer">
-        <img className="logo" src={AppMovies} alt="App Movies" />
-      </a>
-      <a className="redirect" href="https://github.com/VtorVieira/RecipesApp" target="_blank" rel="noreferrer">
-        <img className="logo" src={AppRecipies} alt="App Recipes" />
-      </a>
-      <a className="redirect" href="https://github.com/VtorVieira/GameTrivia" target="_blank" rel="noreferrer">
-        <img className="logo" src={trivia} alt="Trivia Game" />
-      </a>
-      <a className="redirect" href="https://github.com/VtorVieira/BlogsApi" target="_blank" rel="noreferrer">
-        <img className="logo" src={BLOGAPI} alt="Blogs Api" />
-      </a>
-      <a className="redirect" href="https://github.com/VtorVieira/TrybeSmith" target="_blank" rel="noreferrer">
-        <img className="logo" src={trybeSmith} alt="Trybe Smith" />
-      </a>
-      <a className="redirect" href="https://github.com/VtorVieira/Todo-List-React" target="_blank" rel="noreferrer">
-        <img className="logo" src={TODOLIST} alt="Todo List" />
-      </a>
-    </MainProject>
+    <Container>
+      <div className="row">
+        <div className="image">
+          <img src={digitalBank} alt="logo" />
+          <div className="details">
+            <h3>Aplicação FullStack</h3>
+            <p>Nodejs, Express, Postgres, Sequelize, React, Api, Axios, Tailwind, HTML.</p>
+            <div className="links">
+              <a href="https://github.com/VtorVieira/digital-bank" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={poke} alt="logo" />
+          <div className="details">
+            <h3>Aplicação FullStack</h3>
+            <p>Nodejs, Express, Postgres, Sequelize, React, Api, Axios, Tailwind, HTML.</p>
+            <div className="links">
+              <a href="https://pokedex-eta-sooty.vercel.app" target="_blank" rel="noreferrer"><TbPokeball /></a>
+              <a href="https://github.com/VtorVieira/pokedex" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={AppMovies} alt="logo" />
+          <div className="details">
+            <h3>Aplicação FrontEnd</h3>
+            <p>React, Context-Api, Api, Axios, Styled-components, Material-Ui JavaScript, HTML.</p>
+            <div className="links">
+              <a href="https://appmovies-six.vercel.app/" target="_blank" rel="noreferrer"><FiExternalLink /></a>
+              <a href="https://github.com/VtorVieira/AppMovies" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={AppRecipies} alt="logo" />
+          <div className="details">
+            <h3>Aplicação FrontEnd</h3>
+            <p>React, Context-Api, Api, Axios, JavaScript, CSS, HTML.</p>
+            <div className="links">
+              <a href="https://github.com/VtorVieira/RecipesApp" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={trivia} alt="logo" />
+          <div className="details">
+            <h3>Aplicação FrontEnd</h3>
+            <p>React, Redux, Api, Axios, JavaScript, CSS, HTML.</p>
+            <div className="links">
+              <a href="https://github.com/VtorVieira/GameTrivia" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={BLOGAPI} alt="logo" />
+          <div className="details">
+            <h3>Aplicação BackEnd</h3>
+            <p>Nodejs, Express, Sequelize, MySql, JWT, Docker.</p>
+            <div className="links">
+              <a href="https://github.com/VtorVieira/BlogsApi" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={trybeSmith} alt="logo" />
+          <div className="details">
+            <h3>Aplicação BackEnd</h3>
+            <p>Nodejs, TypeScript, Express, Sequelize, MySql, JWT, Docker.</p>
+            <div className="links">
+              <a href="https://github.com/VtorVieira/TrybeSmith" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+        <div className="image">
+          <img src={TODOLIST} alt="logo" />
+          <div className="details">
+            <h3>Aplicação FrontEnd</h3>
+            <p>React, JavaScript, Tailwind, HTML.</p>
+            <div className="links">
+              <a href="https://todo-list-react-flame-tau.vercel.app/" target="_blank" rel="noreferrer"><FiExternalLink /></a>
+              <a href="https://github.com/VtorVieira/Todo-List-React" target="_blank" rel="noreferrer"><FaGithub /></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
   );
 }
 
 export default Projects;
-
-// https://github.com/VtorVieira/GameTrivia
--- src/components/Projects/Style.js
+++ src/components/Projects/Style.js
@@ -1,22 +1,140 @@
 import styled from 'styled-components';
 
-export const MainProject = styled.div`
+export const Container = styled.div`
   /* border: 2px solid #e5e7eb; */
-  display: grid;
-  grid-template-columns: repeat(4,minmax(0,1fr));
-  gap: 1rem;
-  margin-top: 3em;
-  margin-left: 3em;
-  margin-right: 3em;
-  .redirect {
+  margin: 30px;
+  .row {
+    width: 100%;
+    display: grid;
+    grid-template-columns: auto auto auto;
+  }
+
+  .image {
+    background: #50A7FF;
+    position: relative;
+    flex: 1;
+    max-width: 460px;
+    height: 260px;
+    margin: 20px;
+    overflow: hidden;
+  }
+
+  .image img {
+    opacity: 0.8;
+    position: relative;
+    vertical-align: top;
+    transition: 0.6s;
+    transition-property: opacity;
+  }
+
+  .image:hover img {
+    opacity: 0;
+  }
+  
+  .image .details{
+    z-index: 1;
+    position: absolute;
+    top: 0;
+    right: 0;
+    color: #fff;
+    width: 100%;
+    height: 100%;
+  }
+
+  .image .details h3 {
+    text-align: center;
+    font-size: 35px;
+    font-weight: 600;
+    margin-top: 70px;
+    opacity: 0;
+    transition: 0.4s;
+    transition-property: opacity, transform;
+  }
+
+  .image:hover .details h3 {
+    opacity: 1;
+    transform: translateY(-30px)
+  }
+
+  .image .details p {
+    margin: 30px 30px 0 30px;
+    font-size: 25px;
+    font-weight: 600;
+    text-align: center;
+    opacity: 0;
+    transition: 0.6s;
+    transition-property: opacity, transform;
+  }
+
+  .image:hover .details p {
+    opacity: 1;
+    transform: translateY(-30px)
+  }
+
+  .image .details a {
+    font-size: 35px;
+    opacity: 0;
+    transition: 0.6s;
+    transition-property: opacity, transform;
+  }
+
+  .image:hover .details a {
+    opacity: 1;
+    transform: translateY(-40px)
+  }
+
+  .links {
     display: flex;
+    flex-direction: row;
+    /* align-items: center;
+    border-style: dotted; */
+    border: 2;
+  }
+
+  .links a {
+    margin: 0 auto;
+    margin-top: 30px;
+  }
+
+  /* .more {
+    position: absolute;
+    background-color: rgba(255,255,255,0.8);
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
     align-items: center;
-    justify-content: center;
-    border-radius: 5px;
-    .logo {
-      border-radius: 15px;
-      width: 25em;
-      animation: up 1.3s 1;
+    padding: 15px;
+    bottom: -155px;
+    transition: 0.6s;
+    transition-property: bottom;
+  }
+
+  .image:hover .more {
+    bottom: -90px;
+  }
+
+  .more .icon-links {
+    color: #000;
+    font-size: 20px;
+  }
+
+  .more .icon-links a:not(:last-child) i{
+    margin-right: 20px;
+  } */
+
+  @media (max-width: 1080px) {
+    .image{
+      flex: 100%;
+      max-width: 480px;
+    }
+  }
+
+  @media (max-width: 400px) {
+    .image .details p {
+      font-size: 16px;
+    }
+    .more .read-more, .more .icon-links a i{
+      font-size: 18px;
     }
   }
 `
